984ab85344036288a0bfa72a09471c39cee72666
matukai <matt.tanji@gmail.com>
2018-02-19 14:47:14 -1000

    messages angular boiler plate setup

---
+++ public/js/messages.services.js
@@ -0,0 +1,4 @@
+angular.module('myApp')
+.service('MessageService', ['$http', function ($http) {
+  
+}])
--- routes/messages.js
+++ routes/messages.js
@@ -4,10 +4,28 @@ const Message = require('../knex/models/Message');
 
 
 router.post('/', (req,res) => {
-  console.log(req.body)
+  let body = req.body.body;
+  console.log(body)
+  return new Message ({
+    body: body
+    //author_id: 
+    //topic_id:
+  })
+  .save()
+  .then(message => {
+    console.log('ROUTER MESSAGE POST' , message)
+    message = message.toJSON(message);
+  })
+  .catch(err => {
+    console.log(err)
+    console.log('error will robinson')
+  })
 })
 
+router.get('/latest', (req, res) => {
+  console.log('LATEST' , req.body)
 
+})
 
 
 
